389f5a301429d45859f4f696647489879742add3
abisekh subedi <20342543+abisekhsubedi@users.noreply.github.com>
2021-02-07 22:58:08 +0545

    lastest commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,15 @@
-const fontFaceMagic = require("postcss-font-magician");
 module.exports = {
-
-    plugin: [
-        require("cssnano")({
-            preset: "default"
-        }),
-        require("autoprefixer"),
-        require("postcss-pxtorem"),
-        fontFaceMagic({
-            foundries: "google",
-            hosted: ["../src/assets/fonts/Modern-No-20.ttf"]
-        })
+    plugins: [
+        require("cssnano"),
+        // require("postcss-preset-env"),
+        // console.log("cssnano"),
+        require('autoprefixer'),
+        console.log("autoprefixer"),
+        // require("postcss-pxtorem"),
+        // console.log("postcss-pxterm"),
+        // require("postcss-font-magician")({
+            // foundries: "google"
+        // }),
+        // console.log("postcss-font-magician"),
     ]
 }
