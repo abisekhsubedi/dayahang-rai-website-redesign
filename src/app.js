fba1666b37a6f97c2124e18142476f93363bc1f1
abisekh subedi <20342543+abisekhsubedi@users.noreply.github.com>
2021-02-09 14:23:53 +0545

    further fix

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ import "./style.css";
 
 // For DOM manipulation. Navigation menu logic.
 let menuList = document.querySelector(".navbar__menu");
-console.log(menuList);
+// console.log(menuList);
 let menu = document.querySelector("#mobile-menu");
-console.log(menu)
+// console.log(menu)
 
 // Event : toggle menu add .is-active & .active class to menuList & menu
 
@@ -13,18 +13,18 @@ menu.addEventListener("click", () => {
     menu.classList.toggle("is-active");
     menuList.classList.toggle("active");
 
-    console.log(menu.classList, menuList.classList);
+    // console.log(menu.classList, menuList.classList);
 })
 
 // make nav menu disappear ot go back to previous state when navbar menu items are clicked.
 
 let toggleMenu = document.querySelectorAll(".js-toggle-section");
-console.log(toggleMenu)
+// console.log(toggleMenu)
 toggleMenu.forEach((el, i) => {
     el.addEventListener('click', () => {
         menuList.classList.remove("active")
         menu.classList.remove("is-active");
-        console.log(menu)
+        // console.log(menu)
     })
 
 })
